9bbc48999f0fad50082f7a8c3e64377d498ebb60
Steveloper <leesj020925@naver.com>
2024-03-12 23:24:20 +0900

    Feat: Email Verification with Mailgun
    Used Mailgun to send emails but the creating template part
    didn't work.
    Posted the issue and waiting for the reply.

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 import { AuthModule } from './auth/auth.module';
 import { Verification } from './users/entities/verification.entity';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -36,6 +37,9 @@ import { Verification } from './users/entities/verification.entity';
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
         PRIVATE_KEY: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN: Joi.string().required(),
+        MAINGUN_FROM_EMAIL: Joi.string().required(),
       }),
     }),
     //import the TypeORM module. it communiates with Database.
@@ -59,6 +63,11 @@ import { Verification } from './users/entities/verification.entity';
     UserModule,
     JwtModule.forRoot({ privateKey: process.env.PRIVATE_KEY }),
     AuthModule,
+    MailModule.forRoot({
+      apiKey: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+      fromEmail: process.env.MAINGUN_FROM_EMAIL,
+    }),
   ],
   controllers: [],
   providers: [],
